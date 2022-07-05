dd9ca9f5c7824d6b0802e39e95abc7faff151175
Rom Burla <romwebdev@gmail.com>
2022-07-05 22:55:27 +0300

    Progress!!!

--- app/app.js
+++ app/app.js
@@ -33,3 +33,46 @@ lilCasio.style.marginLeft = "5px";
 lilCasio.style.fontFamily =
   "'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif";
 lilCasio.style.fontWeight = "600";
+// end li'l casio
+
+//screen
+container.innerHTML += '<div id="screen">0.</div>';
+let bigScr = document.getElementById("screen");
+bigScr.style.width = "305px";
+bigScr.style.height = "70px";
+bigScr.style.background = "rgb(170,197,214)";
+bigScr.style.background =
+  "linear-gradient(360deg, rgba(170,197,214,1) 15%, rgba(230,239,240,1) 35%)";
+bigScr.style.margin = "8px";
+bigScr.style.display = "flex";
+bigScr.style.justifyContent = "end";
+bigScr.style.alignItems = "flex-end";
+bigScr.style.fontSize = "65px";
+bigScr.style.fontFamily = "Cursive";
+bigScr.style.fontWeight = "600";
+bigScr.style.border = "5px rgb(214, 183, 7) double";
+bigScr.style.borderRadius = "10px";
+//end screen
+
+//blackscreen
+container.innerHTML += '<div id="flblscr"><div id="blackscr"></div></div>';
+let flBlkScr = document.getElementById("flblscr");
+let blkScr = document.getElementById("blackscr");
+//blkscreen flexbox
+flBlkScr.style.display = "flex";
+flBlkScr.style.justifyContent = "end";
+flBlkScr.style.alignItems = "center";
+flBlkScr.style.background = "rgb(236,202,1)";
+flBlkScr.style.background =
+  "linear-gradient(360deg, rgba(236,202,1,1) 30%, rgba(239,205,0,1) 50%)";
+flBlkScr.style.border = "0.5px rgb(214, 183, 7) double";
+flBlkScr.style.height = "35px";
+//end blkscreen flexbox
+
+blkScr.style.marginRight = "7px";
+blkScr.style.width = "80px";
+blkScr.style.height = "25px";
+blkScr.style.backgroundColor = "#433840";
+blkScr.style.border = "2px rgb(214, 183, 7) double";
+blkScr.style.borderRadius = "5px";
+//end black screen
