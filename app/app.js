8955608e179862922b23bd9b0bfe89c96a16feb8
Rom Burla <romwebdev@gmail.com>
2022-07-05 21:36:49 +0300

    headline games

--- app/app.js
+++ app/app.js
@@ -1,3 +1,11 @@
 let root = document.getElementById("root");
 
-root.innerHTML = "<h1>Elazar's beloved calculator</h1>";
+root.innerHTML = '<h1 id = "headline"> Elazars beloved calculator </h1>';
+let headline = document.getElementById("headline");
+headline.style.fontSize = "32px";
+headline.style.fontFamily = "Cursive";
+headline.style.color = "aquamarine";
+headline.style.backgroundColor = "black";
+headline.style.width = "42%";
+headline.style.margin = "auto";
+headline.style.textAlign = "center";
